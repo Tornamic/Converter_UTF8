2415ae5e2e3955ff3b5251a8604b507b9c35aefa
Tornamic <103134732+Tornamic@users.noreply.github.com>
2023-10-02 11:50:58 +0300

    Add files via upload

---
+++ PawnEncodingConverter/PawnEncodingConverter/ConfigReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace PawnEncodingConverter
+{
+    internal class ConfigReader
+    {
+        public string FileName { get; set; }
+        public ConfigReader(string _fileName) 
+        {
+            FileName = _fileName;
+        }
+        public string Read(string name)
+        {
+            foreach (var line in File.ReadAllLines(FileName))
+            {
+                if(line.StartsWith(name)) return line.Remove(0, name.Length + 1);
+            }
+            return String.Empty;
+        }
+    }
+}
---
+++ PawnEncodingConverter/PawnEncodingConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PawnEncodingConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            ConfigReader config = new ConfigReader("encoding.cfg");
+
+            string fileIn = config.Read("FileIn");
+            string fileOut = config.Read("FileOut");
+
+            Console.WriteLine($"Input file (UTF-8) >>> {fileIn}");
+            Console.WriteLine($"Output file (Windows 1251) >>> {fileOut}");
+
+            string fileInText = File.ReadAllText(fileIn, Encoding.UTF8);
+            File.WriteAllText(fileOut, fileInText, Encoding.GetEncoding(1251));
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            File.WriteAllText("exept_pawn_conv.txt", e.ExceptionObject.ToString());
+        }
+    }
+}
